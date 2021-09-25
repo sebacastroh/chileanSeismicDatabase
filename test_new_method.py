aa3c60f7a105a54549e25ed86bf3826aca27e5ba
sebacastroh <sebacastroh@gmail.com>
2021-09-25 19:40:42 -0300

    Add multiprocessing

--- test_new_method.py
+++ test_new_method.py
@@ -4,30 +4,45 @@ import random
 import numpy as np
 import scipy.io as spio
 import correctRecords
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import multiprocessing
 
-path = 'events_mat_uncorrected'
+nTest = 5
 
-with open('p_waves.pkl', 'rb') as f:
-    p_waves = pickle.load(f)
-
-
-n = len(p_waves)
-i = random.randint(0, n)
-while p_waves[i][4] != 'Valid':
-    i = random.randint(0, n)
-
-event = p_waves[i][0]
-station = p_waves[i][1]
-p_loc = p_waves[i][3]
-
-
-mat_event = spio.load(os.path.join(path, event), struct_as_record=False, squeeze_me=True)
-mat_station = mat_event[station]
-
-t = np.linspace(0., (len(mat_station.acc_1)-1)*mat_station.dt, len(mat_station.acc_1))
-
-new_station = correctRecords.correctStation(new_station, p_wave)
+if __name__ == '__main__':
+    path = 'events_mat_uncorrected'
+    
+    with open('p_waves.pkl', 'rb') as f:
+        p_waves = pickle.load(f)
+    
+    
+    n = len(p_waves)
+    selected = []
+    while len(selected) < nTest:
+        i = random.randint(0, n)
+        while p_waves[i][4] != 'Valid':
+            i = random.randint(0, n)
+    
+        event = p_waves[i][0]
+        station = p_waves[i][1]
+        p_loc = p_waves[i][3]
+        group = [event, station, p_loc]
+        
+        if group not in selected:
+            selected.append(group)
+            
+    pool = multiprocessing.Pool(processes=nTest)
+    
+    inputs = []
+    for s in selected:
+        event, station, p_loc = s
+        mat_event = spio.loadmat(os.path.join(path, event), struct_as_record=False, squeeze_me=True)
+        mat_station = mat_event[station]
+        inputs.append([mat_station, p_loc])
+        # t = np.linspace(0., (len(mat_station.acc_1)-1)*mat_station.dt, len(mat_station.acc_1))
+    
+    results = pool.starmap(correctRecords.correctStation, inputs)
+    pool.close()
 
 # fig = plt.figure()
 # a1 = fig.add_subplot(311)
