74db9e7434e563d9d8da310cb7befb24700c0821
sebacastroh <sebacastroh@gmail.com>
2022-10-26 22:03:07 -0300

    Update raw2Uncorrected.py

--- raw2Uncorrected.py
+++ raw2Uncorrected.py
@@ -22,7 +22,7 @@ licensing += 'licensed under the Creative Commons Attribution-'
 licensing += 'NonCommercial-ShareAlike 4.0 International Public License: '
 licensing += 'https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode'
 
-df = pd.read_csv('ZW5TFERBT8B0GMQ.csv')
+df = pd.read_csv('siberrisk.csv', dtype={'Identificador': str})
 
 geod = pyproj.Geod(ellps='WGS84')
 
@@ -37,22 +37,27 @@ with open('fault_plane_properties.json') as f:
 with open('p_waves.json') as f:
     p_waves = json.load(f)
 
-# filenames = os.listdir('rawData')
+xChannels = ['LONGITUDINAL', '350', 'NORTH-SOUTH', '340', '290 DEG', '100',
+             '140', '170', '280', '160', '290', '100 DEGREES', 'L', 'NS', 'N-S',
+             'HNN', 'HN1', 'HLN']
 
-def raw2Uncorrected(filename):
-# for filename in filenames:
-    try:
-        info = df[df['Identificador'] == filename[:-4]].iloc[0]
-    except:
-        # continue
-        return filename
+yChannels = ['TRANSVERSE', '80', 'EAST-WEST', '70', '200 DEG', '190', '10', '080',
+             '50', '200', '010 DEGREES', 'T', 'EW', 'E-W', 'HLE', 'HN2', 'HNE']
+
+zChannels = ['VERTICAL', 'UP', 'V', 'Z', 'NZ', 'HLZ', 'HNZ']
+
+omitChannel = ['INTC']
+
+event_ids = df['ID'].unique()
+
+def raw2Uncorrected(event_id):
+    info = df[df['ID'] == event_id]
     
-    try:
-        event_id = info['Fecha (UTC)'][:10].replace('-', '')
-        event_id += '_' + '%0.1fM' %info['Magnitud [*]']
-        event_id += '_' + '%0.2fS' %np.abs(info['Latitud'])
-        event_id += '_' + '%0.2fW' %np.abs(info['Longitud'])
-        event_id += '_' + '%0.1fKM' %np.abs(info['Profundidad [km]'])
+    st = 0
+    event = {}
+    slab = np.load('sam_slab2.npz')
+    for r, row in info.iterrows():
+        filename = row['Identificador'] + '.npz'
         
         with np.load(os.path.join('rawData', filename), allow_pickle=True) as f:
             data = {}
@@ -64,34 +69,39 @@ def raw2Uncorrected(filename):
         event_lat = info['Latitud']
         event_dep = info['Profundidad [km]']
         
-        # Event type
-        slab = np.load('sam_slab2.npz')
-        dep_pos = np.nanargmin(np.sqrt((slab['lon'] - event_lon)**2 + (slab['lat'] - event_lat)**2))
-        slab_depth = -slab['dep'][dep_pos]
-        difference = slab_depth - event_dep
-        
-        if difference > 10:
-            event_type = 'crustal'
-        elif difference >= -10:
-            event_type = 'interface'
+        if np.any(np.isnan([event_mag, event_lon, event_lat, event_dep])):
+            event_type = 'Undetermined'
+            Rhypo = np.nan
+            Repi  = np.nan
+            Rrup  = np.nan
+            Rjb   = np.nan
+            
+            hypocenter = np.nan
         else:
-            event_type = 'intraslab'
+            # Event type
+            dep_pos = np.nanargmin(np.sqrt((slab['lon'] - event_lon)**2 + (slab['lat'] - event_lat)**2))
+            slab_depth = -slab['dep'][dep_pos]
+            difference = slab_depth - event_dep
             
-        # Distance
-        hypocenter = computeDistances.LatLonDepth2XYZNum(event_lat, event_lon, event_dep)
+            if difference > 10:
+                event_type = 'crustal'
+            elif difference >= -10:
+                event_type = 'interface'
+            else:
+                event_type = 'intraslab'
+                
+            # Distance
+            hypocenter = computeDistances.LatLonDepth2XYZNum(event_lat, event_lon, event_dep)
         
-        event = {}
         for i, (stationCode, station) in enumerate(data.items()):
-            acc_1 = None
-            acc_2 = None
-            acc_3 = None
-            
-            Rhypo = None
-            Repi  = None
-            Rrup  = None
-            Rjb   = None
+            acc_1 = np.empty(0)
+            acc_2 = np.empty(0)
+            acc_3 = np.empty(0)
             
             for channelCode, channel in station.items():
+                if channelCode in omitChannel:
+                    continue
+                
                 location = channel.get('location')
                 if location is None:
                     location = channel.get('loc')
@@ -104,97 +114,102 @@ def raw2Uncorrected(filename):
                 stationStarttime = metadata.get('starttime')
                 stationDt = metadata.get('delta')
                 
-                if channelCode.endswith('N') or channelCode.endswith('1'):
+                if channelCode in xChannels:
                     acc_1 = channel.get('y').copy()
                     component_1 = channelCode
-                elif channelCode.endswith('E') or channelCode.endswith('2'):
+                elif channelCode in yChannels:
                     acc_2 = channel.get('y').copy()
                     component_2 = channelCode
-                else:
+                elif channelCode in zChannels:
                     acc_3 = channel.get('y').copy()
                     component_3 = channelCode
             
             # Distances
-            station_xyz = computeDistances.LatLonDepth2XYZNum(stationLat, stationLon, 0.)
-            if event_mag < 7.1:
-                Rhypo = np.sqrt(np.sum((hypocenter - station_xyz)**2))
-                azimuth1, azimuth2, Repi = geod.inv(stationLon, stationLat, event_lon, event_lat)
-                Repi /= 1000.
-                Rrup = Rhypo
-                Rjb = Repi
-            else:
-                if event_id + '.geojson' in ffm_files:
-                    with open(os.path.join('ffm', event_id + '.geojson')) as f:
-                        ffm = json.load(f)
-                        
-                    max_slip = -np.inf
-                    min_slip =  np.inf
-                    for feature in ffm['features']:
-                        slip = feature['properties']['slip']
-                        max_slip = max(max_slip, slip)
-                        min_slip = min(min_slip, slip)
-                    
-                    dslip = 0.15*(max_slip - min_slip) + min_slip
-                    
+            if not np.isnan(hypocenter):
+                Rhypo = None
+                Repi  = None
+                Rrup  = None
+                Rjb   = None
+                station_xyz = computeDistances.LatLonDepth2XYZNum(stationLat, stationLon, 0.)
+                if event_mag < 7.1:
                     Rhypo = np.sqrt(np.sum((hypocenter - station_xyz)**2))
                     azimuth1, azimuth2, Repi = geod.inv(stationLon, stationLat, event_lon, event_lat)
                     Repi /= 1000.
-                    
-                    Rrup = np.inf
-                    Rjb = np.inf
-                    for feature in ffm['features']:
-                        slip = feature['properties']['slip']
-                        if slip < dslip:
-                            continue
-                        
-                        points = np.array(feature['geometry']['coordinates'][0])[:-1]
-                        sub_fault = computeDistances.LatLonDepth2XYZNumPy(points[:,1], points[:,0], points[:,2]/1000.)
-                        dist = np.sqrt(np.sum((sub_fault - station_xyz)**2, axis=1)).min()
-                            
-                        Rrup = min(Rrup, dist)
-                        
-                        if computeDistances.inPolygon(station_xyz[:2], sub_fault[:,:2]):
-                            Rjb = 0.
-                        elif Rjb != 0.:
-                            azimuth1, azimuth2, dist = geod.inv(stationLon*np.ones(len(points)),\
-                                                                stationLat*np.ones(len(points)),\
-                                                                points[:,0], points[:,1])
-                            Rjb = min(Rjb, dist.min()/1000.)
-                            
+                    Rrup = Rhypo
+                    Rjb = Repi
                 else:
-                    Rhypo = np.sqrt(np.sum((hypocenter - station_xyz)**2))
-                    azimuth1, azimuth2, Repi = geod.inv(stationLon, stationLat, event_lon, event_lat)
-                    Repi /= 1000.
-                    
-                    properties = fpp.get(event_id)
-                    if properties is None:
-                        Rrup = 'Finite fault model required'
-                        Rjb = 'Finite fault model required'
-                    else:
-                        strike, dip, rake = properties
-                        
-                        L = 10**(-2.9  + 0.63*event_mag) # Allen et al 2017, Table 2
-                        W = 10**(-0.86 + 0.35*event_mag) # Allen et al 2017, Table 2
-                        subfaults = computeDistances.generateSubFaults(event_lat, event_lon, event_dep, strike, dip, L, W)
-                        
-                        faultXYZ = computeDistances.LatLonDepth2XYZNumPy(subfaults[:,1],\
-                                                                         subfaults[:,0],\
-                                                                         subfaults[:,2])
+                    if event_id + '.geojson' in ffm_files:
+                        with open(os.path.join('ffm', event_id + '.geojson')) as f:
+                            ffm = json.load(f)
                             
-                        Rrup = np.sqrt(np.sum((faultXYZ - station_xyz)**2, axis=1)).min()
+                        max_slip = -np.inf
+                        min_slip =  np.inf
+                        for feature in ffm['features']:
+                            slip = feature['properties']['slip']
+                            max_slip = max(max_slip, slip)
+                            min_slip = min(min_slip, slip)
                         
-                        nx = int(L)
-                        ny = int(W)
-                        polygon = subfaults[[0,ny-1,nx*ny-1,nx*ny-ny]]
+                        dslip = 0.15*(max_slip - min_slip) + min_slip
                         
-                        if computeDistances.inPolygon([stationLon, stationLat], polygon):
-                            Rjb = 0.
-                        else:
-                            azimuth1, azimuth2, dist = geod.inv(stationLon*np.ones(nx*ny),\
-                                                                stationLat*np.ones(nx*ny),\
-                                                                subfaults[:,0], subfaults[:,1])
+                        Rhypo = np.sqrt(np.sum((hypocenter - station_xyz)**2))
+                        azimuth1, azimuth2, Repi = geod.inv(stationLon, stationLat, event_lon, event_lat)
+                        Repi /= 1000.
+                        
+                        Rrup = np.inf
+                        Rjb = np.inf
+                        for feature in ffm['features']:
+                            slip = feature['properties']['slip']
+                            if slip < dslip:
+                                continue
+                            
+                            points = np.array(feature['geometry']['coordinates'][0])[:-1]
+                            sub_fault = computeDistances.LatLonDepth2XYZNumPy(points[:,1], points[:,0], points[:,2]/1000.)
+                            dist = np.sqrt(np.sum((sub_fault - station_xyz)**2, axis=1)).min()
                                 
-                            Rjb = dist.min()/1000.
+                            Rrup = min(Rrup, dist)
+                            
+                            if computeDistances.inPolygon(station_xyz[:2], sub_fault[:,:2]):
+                                Rjb = 0.
+                            elif Rjb != 0.:
+                                azimuth1, azimuth2, dist = geod.inv(stationLon*np.ones(len(points)),\
+                                                                    stationLat*np.ones(len(points)),\
+                                                                    points[:,0], points[:,1])
+                                Rjb = min(Rjb, dist.min()/1000.)
+                                
+                    else:
+                        Rhypo = np.sqrt(np.sum((hypocenter - station_xyz)**2))
+                        azimuth1, azimuth2, Repi = geod.inv(stationLon, stationLat, event_lon, event_lat)
+                        Repi /= 1000.
+                        
+                        properties = fpp.get(event_id)
+                        if properties is None:
+                            Rrup = 'Finite fault model required'
+                            Rjb = 'Finite fault model required'
+                        else:
+                            strike, dip, rake = properties
+                            
+                            L = 10**(-2.9  + 0.63*event_mag) # Allen et al 2017, Table 2
+                            W = 10**(-0.86 + 0.35*event_mag) # Allen et al 2017, Table 2
+                            subfaults = computeDistances.generateSubFaults(event_lat, event_lon, event_dep, strike, dip, L, W)
+                            
+                            faultXYZ = computeDistances.LatLonDepth2XYZNumPy(subfaults[:,1],\
+                                                                             subfaults[:,0],\
+                                                                             subfaults[:,2])
+                                
+                            Rrup = np.sqrt(np.sum((faultXYZ - station_xyz)**2, axis=1)).min()
+                            
+                            nx = int(L)
+                            ny = int(W)
+                            polygon = subfaults[[0,ny-1,nx*ny-1,nx*ny-ny]]
+                            
+                            if computeDistances.inPolygon([stationLon, stationLat], polygon):
+                                Rjb = 0.
+                            else:
+                                azimuth1, azimuth2, dist = geod.inv(stationLon*np.ones(nx*ny),\
+                                                                    stationLat*np.ones(nx*ny),\
+                                                                    subfaults[:,0], subfaults[:,1])
+                                    
+                                Rjb = dist.min()/1000.
             
             # Station properties
             properties = sinfo.get(stationCode)
@@ -250,13 +265,12 @@ def raw2Uncorrected(filename):
                 'azimuth': azimuth,
                 'licensing': licensing
             }
-            event['st%0.2i' %i] = station_dict
+            event['st%0.2i' %st] = station_dict
+            st += 1
         
-        slab.close()
         np.savez_compressed(os.path.join('databaseUncorrected', 'npz', event_id), **event)
         spio.savemat(os.path.join('databaseUncorrected', 'mat', event_id + '.mat'), event, do_compression=True)
-    except:
-        return filename
+    slab.close()
     
 if __name__ == '__main__':
     filenames = os.listdir('rawData')
